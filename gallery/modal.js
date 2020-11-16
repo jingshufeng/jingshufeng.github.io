5b1e229587135bc80fbdfe87cc66216d3803846e
Jingshu Feng <jingshufeng0520@gmail.com>
2020-11-16 16:45:36 -0500

    undate javascript and html

--- gallery/modal.js
+++ gallery/modal.js
@@ -1,5 +1,28 @@
 //切换toggle: document.getElementsByClassName("原本的className")[0].classList.toggle("新的样式名称");//
 
+//写一个修改图片们HTML的拼串儿函数
+function getImagesHTML(img, description) {
+    return "<div class='gallery'>" +
+        "<a href='" + img + "'>" +
+        "<img src='" + img + "'>" +
+        "</a>" +
+        "<div class='description'><a href='" + img + "'>" + description + "</a></div>" +
+        "</div>"
+}
+//创建一个变量用来保存所有的HTML
+var allHTML = "";
+//for循环来增加图片
+for (var i = 0; i < images.length; i++) {
+    //var image = images[i];
+    //var img = image[0];
+    //var description = image[1];
+    allHTML += getImagesHTML(images[i][0], images[i][1]);
+}
+//修改原网页中的HTML
+var galleryContainer = document.getElementById("galleryContainer");
+galleryContainer.innerHTML = allHTML;
+
+
 //首先获取modal和modalCloseBtn
 var modal = document.getElementsByClassName("modal")[0];
 var modalCloseBtn = document.getElementsByClassName("modalCloseBtn")[0];
@@ -22,3 +45,25 @@ for (var i = 0; i < galleries.length; i++) {
         e.preventDefault();
     };
 }
+
+//首先获取两个按钮
+// basic paging logic to demo the buttons
+var pr = document.querySelector('.paginate.left');
+var pl = document.querySelector('.paginate.right');
+
+pr.onclick = slide.bind(this, -1);
+pl.onclick = slide.bind(this, 1);
+
+var index = 0, total = 5;
+
+function slide(offset) {
+    index = Math.min(Math.max(index + offset, 0), total - 1);
+
+    document.querySelector('.counter').innerHTML = (index + 1) + ' / ' + total;
+
+    pr.setAttribute('data-state', index === 0 ? 'disabled' : '');
+    pl.setAttribute('data-state', index === total - 1 ? 'disabled' : '');
+}
+
+slide(0);
+
